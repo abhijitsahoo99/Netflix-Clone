81a47a74e8835ac0e37f6318540466268956c925
abhijitsahoo99 <abhijitsahooas.01@gmail.com>
2023-10-27 16:31:41 +0530

    Featured design page added

--- netflix-clone/src/pages/home/Home.jsx
+++ netflix-clone/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import Featured from '../../components/Featured/Featured'
 
 function home() {
   return (
     <div>
       <Navbar  />
+      <Featured type="movie" />
     </div>
   )
 }
